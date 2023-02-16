2f9f04a70c6b4802f17094ca088aea04cdaffc49
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-02-16 13:11:11 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A code that outputs out a line with puts func.
+ * Return: 0 (success)
+ */
+
+in main(void)
+{
+	puts(" \" Programming is like a building multilingual puzzle");
+}
