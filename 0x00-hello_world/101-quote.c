a0a765c4ba9b1eda0183aa53c320e10e1927b014
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-02-17 07:18:02 -0800

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - startin  point
- *
+ * Dora Korpar, 2015-10-19, followed by a line, to the standard error
  * Return: when always 0 (Success)
 */
 
 int main(void)
 {
-	write('and that piece of art is useful" -Dora Korpar, 2015-10-19 \n', 61);
+	write(2, 'and that piece of art is useful\" -Dora Korpar, 2015-10-19\n', 59);
 	return (1);
 }
