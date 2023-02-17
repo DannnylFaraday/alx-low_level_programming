3f093335aa72d039f138af4c4c216100335e1f6e
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-02-16 20:29:38 -0800

    new_tiring_projects

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main-A code that prints out a line using the printf function
+ * Return 0 means  it's a success
+ */
+int main (void){
+printf("with a proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
