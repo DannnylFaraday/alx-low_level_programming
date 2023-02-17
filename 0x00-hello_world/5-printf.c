77e274b69af9b550e2ce8968b304920d4503c8be
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-02-16 20:35:24 -0800

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main-A code that prints out a line using the printf function
  * Return 0 means  it's a success
  */
-int main (void){
-printf("with a proper grammar, but the outcome is a piece of art,\n");
-return (0);
+int main (void)
+{
+	printf("with a proper grammar, but the outcome is a piece of art,\n");
+Return (0);
 }
