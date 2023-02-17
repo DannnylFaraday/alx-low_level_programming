ccf59d77559576b51a4fc9da6693c765451c03bb
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-02-16 20:51:00 -0800

    finally

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main-A code that prints out a line using the printf function
+ * main - A code that prints out a line using the printf function
  * Return 0 means  it's a success
  */
 
 int main(void)
 {
 	printf("with a proper grammar, but the outcome is a piece of art,\n");
-return (0);
+Return (0);
 }
+
+
