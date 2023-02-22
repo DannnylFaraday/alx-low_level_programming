539455874535b8f9b39646d9caa0fddd91f1b12b
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-02-22 06:14:54 -0800

    hey

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	for(i <= 10)
+	for(i <= 10;)
 	{
-		for (c = 'a'; c <= 'z'; c++){
+		for (c = 'a'; c <= 'z'; c++;){
 			_putchar(c);
 		}
 		i++;
