3ac988eb9f6ebfdd7308ba2e1a83e18681703bf0
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-03-02 14:06:05 -0800

    hey,there

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,13 +13,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = -num;
 	}
 
-	x = n;
+	if ((x / 10) > 0 )
+		print_number(num / 10);
 
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	_putchar(num % 10 + '0');
 }
