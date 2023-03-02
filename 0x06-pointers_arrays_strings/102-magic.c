aa225dc689a2c7948c2621ce2f0b45266924ce75
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-03-02 14:38:37 -0800

    casper, got ya!

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,25 @@
-#include <stdio.h>
-
-
- /*
-	I used every magic trick up my sleeves to make this trick work!
-        WARNING: Don't try to edit code as it might not work.
-	All blocks of codes are cut and trimed in perfection	
-   */
-int main(void)
-{
-  int n;
-  int a[5];
-  int *p;
-
-  a[2] = 1024;
-  p = &n;
-    *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+#include <stdio.h>                                                                                                                    
+                                                                                                                                      
+int main(void)                                                                                                                        
+{                                                                                                                                     
+        int n;                                                                                                                        
+        int a[5];                                                                                                                     
+        int *p;                                                                                                                       
+                                                                                                                                      
+        a[2] = 1024;                                                                                                                  
+        p = &n;                                                                                                                       
+/*                                                                                                                                    
+ * write your line of code here...                                                                                                    
+ * Remember:                                                                                                                          
+ * main - magic                                                                                                                       
+ * - you are not allowed to use a                                                                                                     
+ *   - you are not allowed to modify p                                                                                                
+ *   - only one statement                                                                                                             
+ *   - you are not allowed to code anything else than this line of code                                                               
+ */                                                                                                                                   
+        *(p + 5) = 98;                                                                                                                
+/* ...so that this prints 98\n */                                                                                                     
+        printf("a[2] = %d\n", a[2]);                                                                                                  
+        return (0);                                                                                                                   
 }
+
