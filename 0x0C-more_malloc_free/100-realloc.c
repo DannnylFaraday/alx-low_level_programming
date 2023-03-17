086b0ab0bcea1569485db3890b9dabd55600e4b1
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-03-16 18:57:45 -0700

    .

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,19 +1,39 @@
 #include "main.h"
 
 /**
- * malloc_checked - cause normal process termination with a status value of 98
+ * _realloc - reallocates a memory block using malloc and free
  *
- * @b: allocated memory
+ * @ptr: pointer to the memory previously allocated
+ * @old_size:is the size, in bytes, of the allocated space for ptr
+ * @new_size: the new size, in bytes of the new memory block
  *
- * Return: a pointer to the allocated memory
+ * Return: pointer allocate new size memory, or NULL
  */
-void *malloc_checked(unsigned int b)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *i;
+	char *p;
+	unsigned int i, n = new_size;
+	char *oldp = ptr;
 
-	i = malloc(b);
-	if (i == NULL)
-		exit(98);
-	return (i);
-
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		n = old_size;
+	for (i = 0; i < n; i++)
+		p[i] = oldp[i];
+	free(ptr);
+	return (p);
 }
