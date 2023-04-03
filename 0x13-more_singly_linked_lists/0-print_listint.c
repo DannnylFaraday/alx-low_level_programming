01f80a2722ea713c503b642236c2a277b6b2d544
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-04-03 14:42:52 -0700

    listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,12 +3,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count =0;
+	size_t count = 0;
 	
-	while (h ! = NULL)
+	while (h != NULL)
 	{
 		printf("&d\n", h->n);
 		h = h->next;
 		count = count + 1;
 	}
+	return (count);
 }
