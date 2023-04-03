ca66926b327a5042c31829c0c24232fc44070668
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-04-03 15:04:27 -0700

    lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
+ * n:serves as integer
+ * next: points to the next node
+ * All bout node structure
  */
-
-#include <stddef.h>
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+  	int n;
+  	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
