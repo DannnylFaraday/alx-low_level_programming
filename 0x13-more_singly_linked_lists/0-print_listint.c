f669a645530cb93ccd79c2f48c09c56d3075d37f
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-04-03 14:49:11 -0700

    listinter

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,18 @@
 #include "lists.h"
 #include <stdio.h>
 
+/*
+ * print_listint : is used for listing the objects and properties of listint
+ * *h is the node
+ * Return number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 	
 	while (h != NULL)
 	{
-		printf("&d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		count = count + 1;
 	}
