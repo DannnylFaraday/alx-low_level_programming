1183441595df4ccc55085e0a75d46a346883b245
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-04-03 16:47:19 -0700

    Xtras

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 
 /**
- * _r - changes the memory for an array of pointers to the nodes in a linked list
- * @list: the old list  changes
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
  * @size: size of the new list (always one more than the old list)
- * @new: new node added to the list
+ * @new: new node to add to the list
+ *
  * Return: pointer to the new list
  */
 const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
@@ -29,7 +31,8 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 
 /**
  * print_listint_safe - prints a listint_t linked list.
- * @head: points to the starting of the list
+ * @head: pointer to the start of the list
+ *
  * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
