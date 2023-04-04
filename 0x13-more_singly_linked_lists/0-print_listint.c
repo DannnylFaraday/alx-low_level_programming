1c215843fc0158715c7cd8aa1a82b7e83b9870f0
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-04-04 15:50:53 -0700

    append

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,8 +3,11 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list.
+ *
  * @h: head of linklist node
+ *
  * Return: the number of nodes
+ *
  */
 size_t print_listint(const listint_t *h)
 {
