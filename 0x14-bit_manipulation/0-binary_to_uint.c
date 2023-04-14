baae7bbcaf8dfa13bafafb334f6e9ad90880f17a
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-04-14 12:50:04 -0700

    bin

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,30 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: The converted number, or 0 if there is one or more chars in the
- *         string b that is not 0 or 1, or if b is NULL.
+ * binary_to_uint - Functions converts binary number
+ * @b: A pointer to binary string
+ * return: unsigned integer converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int num = 0;
+	unsigned int base = 1, result = 0, len = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	
-	for (i = 0; b[i] != '\0'; i++ ) //loops each characher in the set of strings
+	while (b[len])
+		len = len +1;
+
+	while (len)
 	{
-		
-		if (b[i] != '0' && b[i] != '1') //returns 0 if and only if the characters are true or false
+		if (b[len - 1] != '0' && b[len - 1] != '1')
 			return (0);
+
+		if (b[len - 1] == '1')
+			result += base;
+		base *= 2;
+		len = len - 1;
 	}
-
-	
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		num << 1; /* Left shift by 1, by eqn x*(2^n) which n is 1*/
-
-		if (b[i] == '1')
-			num += 1;
-	}
-
-	return (num);
+	return (result);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
 
-unsigned long int _power(unsigned int base, unsigned int pow);
+/*
+ * File: main.h
+ *containing all prototypes of all the functions
+ */
+
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,7 +17,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-int _putchar(char c);
 
-
-#endif
+#endif 
