c096c1a6f86b312baf91cca524e8ac36ff6ea776
Ayomide <ayomide.daniel.adeosun@gmail.com>
2023-04-14 19:21:40 -0700

    create_file...

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,38 @@
 #include "main.h"
 
 /**
- * filename: The file created.
-* create_file - creates a file with the given text content. 
-* text_content:  Null terminated the string to write to the file.
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content written in the source file.
  *
- * If returns 1 its a success, otherwise -1 on failure.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written = 0;
-	size_t len = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
-	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	if (len > 0)
-		bytes_written = write(fd, text_content, len);
-	close(fd);
+	if (!text_content)
+		text_content = "";
 
-	if (bytes_written == -1)
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
+
+	close(fd);
 
 	return (1);
 }
